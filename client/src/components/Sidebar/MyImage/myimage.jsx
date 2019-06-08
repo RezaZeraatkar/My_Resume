0ddf90aaa186926ae130b8da1adbeef2d2c86126
Reza Zeraatkar <r.zeraatkar1992@gmail.com>
2019-06-08 16:38:22 -0400

    image for mobile devices stilized

--- client/src/components/Sidebar/MyImage/myimage.jsx
+++ client/src/components/Sidebar/MyImage/myimage.jsx
@@ -7,8 +7,8 @@ import "./myimage.scss";
 import Myimage from "./avatar_hat.jpg";
 
 const MyImage = () => (
-  <div className="image-container img-dark-inset-gradient">
-    <img src={Myimage} style={{ width: "100%" }} alt="myimage" />
+  <div className="container">
+    <img src={Myimage} className="img-round" alt="myimage" />
     <div className="display-bottomleft container text-white">
       <h2>Reza Zeraatkar</h2>
     </div>
