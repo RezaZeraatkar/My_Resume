6e728386e72579d22c3dfc5f7e4bcb0c24cf79df
Reza Zeraatkar <r.zeraatkar1992@gmail.com>
2019-08-21 09:23:45 -0400

    travis ci test

---
+++ client/src/components/CardBody/__test__/cardbody.test.js
@@ -0,0 +1,9 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import CardBody from "..cardBody/";
+
+// it("renders without crashing", () => {
+//   const div = document.createElement("div");
+//   ReactDOM.render(<CardBody />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
