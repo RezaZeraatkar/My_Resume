cfd109f1520378f8e52e216d401eb893b2bfa104
Reza Zeraatkar <r.zeraatkar1992@gmail.com>
2019-08-22 04:44:39 -0400

    Deploy on Heroku without TravisCI just For Test

--- server/database/database.js
+++ server/database/database.js
@@ -1,9 +1,12 @@
+/* eslint-disable no-multi-spaces */
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
 const env = process.env.NODE_ENV || "development";
-const MongoURI = process.env.MONGODB_URI || `mongodb://localhost/mern_${env}`;
+// eslint-disable-next-line prettier/prettier
+const MongoURI =
+  process.env.MONGODBLAB_URI || `mongodb://localhost/mern_${env}`;
 
 module.exports = {
   mongoose,
